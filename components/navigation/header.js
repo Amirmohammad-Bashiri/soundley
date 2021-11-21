8f2d6bbb3cbbd91f10597a8c395d3cb6cbc12be1
Amirmohammad-Bashiri <amirmohamad.bashiri@gmail.com>
2021-11-21 12:56:44 +0330

    Removed react responsive package

--- components/navigation/header.js
+++ components/navigation/header.js
@@ -1,12 +1,7 @@
 import Link from "next/link";
-import { useMediaQuery } from "react-responsive";
 import { SearchIcon, UserCircleIcon } from "@heroicons/react/solid";
 
 function Header() {
-  const isDesktopOrLaptop = useMediaQuery({
-    query: "(min-width: 1124px)",
-  });
-
   return (
     <header className="flex items-center justify-center px-10 py-6 xl:justify-between">
       {/* search bar */}
@@ -20,18 +15,16 @@ function Header() {
       </div>
 
       {/* avatar and name */}
-      {isDesktopOrLaptop ? (
-        <div className="flex items-center space-x-6 text-gray-100 bg-gray-700 rounded-md focus-within:border-2 focus-within:border-gray-200">
-          <div className="p-2 bg-gray-600 rounded-md">
-            <UserCircleIcon className="w-7 h-7" />
-          </div>
-          <div className="pr-5 text-lg font-bold text-gray-50">
-            <Link href="/login">
-              <a>Login</a>
-            </Link>
-          </div>
+      <div className="items-center hidden space-x-6 text-gray-100 bg-gray-700 rounded-md xl:flex focus-within:border-2 focus-within:border-gray-200">
+        <div className="p-2 bg-gray-600 rounded-md">
+          <UserCircleIcon className="w-7 h-7" />
         </div>
-      ) : null}
+        <div className="pr-5 text-lg font-bold text-gray-50">
+          <Link href="/login">
+            <a>Login</a>
+          </Link>
+        </div>
+      </div>
     </header>
   );
 }
--- containers/layout.js
+++ containers/layout.js
@@ -1,19 +1,12 @@
-import { useMediaQuery } from "react-responsive";
 import Header from "@components/navigation/header";
 import Sidebar from "@components/navigation/sidebar";
 
 function Layout({ children }) {
-  const isDesktopOrLaptop = useMediaQuery({
-    query: "(min-width: 1124px)",
-  });
-
   return (
     <div className="grid h-screen grid-cols-5 border-r-2 xl:grid-cols-7">
-      {isDesktopOrLaptop ? (
-        <div>
-          <Sidebar />
-        </div>
-      ) : null}
+      <div className="hidden xl:block">
+        <Sidebar />
+      </div>
 
       <div className="col-span-5 bg-gray-900 xl:col-span-6">
         <Header />
