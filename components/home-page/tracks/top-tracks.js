95148310e16c1b9da989282bf201271cf9a86344
Amirmohammad-Bashiri <amirmohamad.bashiri@gmail.com>
2021-11-27 18:41:49 +0330

    Responsive top tracks section

--- components/home-page/tracks/top-tracks.js
+++ components/home-page/tracks/top-tracks.js
@@ -13,31 +13,32 @@ function TopTracks() {
     <ul className="px-6 pb-6 space-y-6">
       {data.map(track => (
         <li className="flex items-center justify-between" key={track.id}>
-          <div className="flex items-center space-x-4">
-            <div className="relative">
+          <div className="flex items-center space-x-5">
+            <div
+              className="relative w-16 h-16"
+              style={{ minHeight: "4rem", minWidth: "4rem" }}>
               <Image
                 src={track.artist.picture_medium}
                 alt={track.title}
-                width={64}
-                height={64}
+                layout="fill"
                 className="rounded"
               />
               <div className="absolute top-0 left-0 w-16 h-16 rounded opacity-20 border-gray-50 bg-gradient-to-r from-blue-500 via-indigo-800 to-purple-800"></div>
             </div>
-            <div className="flex flex-col items-center space-y-2">
-              <strong className="text-gray-100 line-clamp-1">
+            <div className="flex flex-col justify-center space-y-2">
+              <strong className="text-sm text-gray-100 line-clamp-1 md:text-lg">
                 {track.title}
               </strong>
               <Link href={`/artists/${track.artist.id}`}>
-                <a className="text-sm text-gray-300 line-clamp-1">
+                <a className="text-xs text-gray-300 md:text-sm line-clamp-1">
                   {track.artist.name}
                 </a>
               </Link>
             </div>
           </div>
 
-          <div className="flex items-center space-x-6">
-            <time className="font-semibold text-gray-200">
+          <div className="flex items-center space-x-3 md:space-x-6">
+            <time className="text-sm font-semibold text-gray-200">
               {convertTrackDuration(track.duration)}
             </time>
             <button className="p-1 bg-gray-800 border border-gray-400 rounded">
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,19 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontSize: {
+      xs: ".75rem",
+      sm: ".875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
+      "7xl": "5rem",
+    },
   },
   variants: {
     extend: {},
