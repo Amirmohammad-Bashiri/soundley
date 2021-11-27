f37eb170bf05fcef51b980901c43f794a3a5d2ea
Amirmohammad-Bashiri <amirmohamad.bashiri@gmail.com>
2021-11-27 18:57:06 +0330

    Fixed top track duration numbers

--- utils/track-duration-converter.js
+++ utils/track-duration-converter.js
@@ -2,6 +2,10 @@ export function convertTrackDuration(duration) {
   const minutes = Math.floor(duration / 60);
   const seconds = duration - minutes * 60;
   const convertedDuration = `${minutes}:${seconds}`;
+  const result =
+    convertedDuration.length === 3
+      ? convertedDuration + "0"
+      : convertedDuration;
 
-  return convertedDuration;
+  return result;
 }
