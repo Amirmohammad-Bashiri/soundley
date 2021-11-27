98d761e1f700fb97f03edb37cb26e1c6532008d3
Amirmohammad-Bashiri <amirmohamad.bashiri@gmail.com>
2021-11-27 16:18:47 +0330

    Added view all link to top albums

--- containers/home-page/grid-container.js
+++ containers/home-page/grid-container.js
@@ -35,9 +35,16 @@ function GridContainer() {
       </section>
 
       <section className="relative overflow-y-scroll bg-gray-800 rounded xl:order-2 2xl:order-none xl:row-span-3 xl:col-span-4 2xl:col-span-3 max-h-80">
-        <h2 className="sticky top-0 z-10 py-4 pl-6 text-xl font-semibold tracking-wide bg-gray-800 md:text-2xl text-gray-50">
-          Top Albums
-        </h2>
+        <div className="sticky top-0 z-10 flex items-center justify-between py-4 mb-6 bg-gray-800">
+          <h2 className="pl-6 text-xl font-semibold tracking-wide md:text-2xl text-gray-50">
+            Top Albums
+          </h2>
+          <Link href="/albums">
+            <a className="pr-6 text-sm font-medium text-gray-300 md:text-base">
+              View All
+            </a>
+          </Link>
+        </div>
 
         <ErrorBoundary
           fallbackRender={({ resetErrorBoundary }) => (
