88c0c97676780cf3b58b856072e8261c1d2bc550
Amirmohammad-Bashiri <amirmohamad.bashiri@gmail.com>
2021-11-28 12:25:49 +0330

    Homepage desktop player setup

--- pages/_document.js
+++ pages/_document.js
@@ -25,6 +25,19 @@ class MyDocument extends Document {
               href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap"
             />
           </noscript>
+
+          <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.15.4/css/solid.css"
+            integrity="sha384-Tv5i09RULyHKMwX0E8wJUqSOaXlyu3SQxORObAI08iUwIalMmN5L6AvlPX2LMoSE"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.15.4/css/fontawesome.css"
+            integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7"
+            crossOrigin="anonymous"
+          />
         </Head>
         <body>
           <Main />
