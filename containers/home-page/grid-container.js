ca076e57d9439acdb6fde104a009df9a753028eb
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-12-07 10:28:38 +0330

    Removed view all links from albums and top tracks

--- containers/home-page/grid-container.js
+++ containers/home-page/grid-container.js
@@ -11,16 +11,9 @@ function GridContainer() {
   return (
     <div className="home-grid-layout">
       <section className="relative px-4 py-4 bg-gray-800 rounded xl:order-1 2xl:order-none xl:col-span-8 xl:row-span-3 xl:px-6">
-        <div className="flex items-center justify-between mb-6">
-          <h2 className="text-xl font-semibold tracking-wide md:text-2xl text-gray-50">
-            Top Artists
-          </h2>
-          <Link href="/artists">
-            <a className="text-sm font-medium text-gray-300 md:text-base">
-              View All
-            </a>
-          </Link>
-        </div>
+        <h2 className="mb-6 text-xl font-semibold tracking-wide md:text-2xl text-gray-50">
+          Top Artists
+        </h2>
         <ErrorBoundary
           fallbackRender={({ resetErrorBoundary }) => (
             <ErrorFallback
@@ -37,16 +30,9 @@ function GridContainer() {
       </section>
 
       <section className="relative overflow-y-scroll bg-gray-800 rounded xl:order-2 2xl:order-none xl:row-span-3 xl:col-span-4 2xl:col-span-3 max-h-80">
-        <div className="sticky top-0 z-10 flex items-center justify-between py-4 mb-6 bg-gray-800">
-          <h2 className="pl-6 text-xl font-semibold tracking-wide md:text-2xl text-gray-50">
-            Top Albums
-          </h2>
-          <Link href="/albums">
-            <a className="pr-6 text-sm font-medium text-gray-300 md:text-base">
-              View All
-            </a>
-          </Link>
-        </div>
+        <h2 className="sticky top-0 z-10 py-4 pl-6 mb-6 text-xl font-semibold tracking-wide bg-gray-800 md:text-2xl text-gray-50">
+          Top Albums
+        </h2>
 
         <ErrorBoundary
           fallbackRender={({ resetErrorBoundary }) => (
@@ -60,16 +46,9 @@ function GridContainer() {
       </section>
 
       <section className="relative overflow-y-scroll bg-gray-800 rounded xl:order-4 2xl:order-none xl:row-span-3 xl:col-span-7 2xl:col-span-5 max-h-80">
-        <div className="sticky top-0 z-10 flex items-center justify-between py-4 mb-6 bg-gray-800">
-          <h2 className="pl-6 text-xl font-semibold tracking-wide md:text-2xl text-gray-50">
-            Top Tracks
-          </h2>
-          <Link href="/tracks">
-            <a className="pr-6 text-sm font-medium text-gray-300 md:text-base">
-              View All
-            </a>
-          </Link>
-        </div>
+        <h2 className="sticky top-0 z-10 py-4 pl-6 mb-6 text-xl font-semibold tracking-wide bg-gray-800 md:text-2xl text-gray-50">
+          Top Tracks
+        </h2>
 
         <ErrorBoundary
           fallbackRender={({ resetErrorBoundary }) => (
