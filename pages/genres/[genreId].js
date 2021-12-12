1a55368fac959c387baf9ce7173b69d6292b4320
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-12-12 11:26:14 +0330

    Implemented server side data fetching for genre item

--- pages/genres/[genreId].js
+++ pages/genres/[genreId].js
@@ -1,4 +1,8 @@
 import { useRouter } from "next/router";
+import { QueryClient, dehydrate } from "react-query";
+import { deezerClient } from "@clients/deezer-client";
+
+import { getGenreItem } from "@lib/genres";
 
 function GenreItemPage() {
   const { query } = useRouter();
@@ -8,6 +12,26 @@ function GenreItemPage() {
       <h1 className="text-white">Genre ID: {query.genreId}</h1>
     </main>
   );
+}
+
+export async function getServerSideProps(ctx) {
+  const { query } = ctx;
+
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(
+    ["genres", query.genreId],
+    () => getGenreItem(deezerClient, `/genre/${query.genreId}/artists`),
+    {
+      staleTime: 1000 * 60 * 60 * 24,
+    }
+  );
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
 }
 
 export default GenreItemPage;
