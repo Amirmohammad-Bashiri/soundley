bf12642a60d2fd3e995925498c7e0f21f9c87310
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-12-13 10:45:01 +0330

    Implemented music popup animation

--- components/global-player/music-player-popup.js
+++ components/global-player/music-player-popup.js
@@ -1,12 +1,40 @@
+import { motion } from "framer-motion";
+
 import { useMusicPlayerPopup } from "@store/music-player-popup-context";
+
+const dropIn = {
+  hidden: {
+    y: "100vh",
+    opacity: 0,
+  },
+  visible: {
+    y: "0",
+    opacity: 1,
+    transition: {
+      duration: 0.3,
+      type: "spring",
+      damping: 50,
+      stiffness: 500,
+    },
+  },
+  exit: {
+    y: "100vh",
+    opacity: 0,
+  },
+};
 
 function MusicPlayerPopup() {
   const { togglePopup } = useMusicPlayerPopup();
 
   return (
-    <div className="fixed top-0 left-0 z-30 w-full h-full bg-blue-500">
+    <motion.div
+      variants={dropIn}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+      className="fixed top-0 left-0 z-30 w-full h-full bg-blue-500">
       <button onClick={togglePopup}>close</button>
-    </div>
+    </motion.div>
   );
 }
 
--- hoc/music-player-portal.js
+++ hoc/music-player-portal.js
@@ -1,13 +1,25 @@
 import { createPortal } from "react-dom";
+import { AnimatePresence } from "framer-motion";
 
 import { useMusicPlayerPopup } from "@store/music-player-popup-context";
 
 const MusicPlayerPortal = ({ children }) => {
   const { isPopupOpen } = useMusicPlayerPopup();
 
-  return isPopupOpen
-    ? createPortal(children, document.getElementById("music-portal"))
-    : null;
+  if (typeof document !== "undefined") {
+    return createPortal(
+      <AnimatePresence exitBeforeEnter>
+        {isPopupOpen ? children : null}
+      </AnimatePresence>,
+      document.getElementById("music-portal")
+    );
+  } else {
+    return null;
+  }
+
+  // return isPopupOpen
+  //   ? createPortal(children, document.getElementById("music-portal"))
+  //   : null;
 };
 
 export default MusicPlayerPortal;
