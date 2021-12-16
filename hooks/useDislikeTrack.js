5289fda69eea70c89a50e46a475e6985a9663153
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-12-16 15:11:44 +0330

    Implemented optimistic update for like and dislike

--- hooks/useDislikeTrack.js
+++ hooks/useDislikeTrack.js
@@ -6,7 +6,20 @@ export function useDislikeTrack() {
   const queryClient = useQueryClient();
 
   return useMutation(dislikeTrack, {
-    onSuccess: () => {
+    onMutate: async trackId => {
+      await queryClient.cancelQueries("user");
+      const previousValue = queryClient.getQueriesData("user");
+
+      queryClient.setQueryData("user", old => ({
+        ...old,
+        likes: old.likes.filter(track => track.id !== trackId),
+      }));
+
+      return previousValue;
+    },
+    onError: (err, variables, previousValue) =>
+      queryClient.setQueryData("user", previousValue),
+    onSettled: () => {
       queryClient.invalidateQueries("user");
     },
   });
--- hooks/useLikeTrack.js
+++ hooks/useLikeTrack.js
@@ -6,7 +6,20 @@ export function useLikeTrack() {
   const queryClient = useQueryClient();
 
   return useMutation(likeTrack, {
-    onSuccess: () => {
+    onMutate: async track => {
+      await queryClient.cancelQueries("user");
+      const previousValue = queryClient.getQueryData("user");
+
+      queryClient.setQueryData("user", old => ({
+        ...old,
+        likes: [...old.likes, track],
+      }));
+
+      return previousValue;
+    },
+    onError: (err, variables, previousValue) =>
+      queryClient.setQueryData("user", previousValue),
+    onSettled: () => {
       queryClient.invalidateQueries("user");
     },
   });
