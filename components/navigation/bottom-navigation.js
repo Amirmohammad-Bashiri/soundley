0a2532bed24c6160af1ee2429fe327ccc055c299
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-12-16 10:19:45 +0330

    Added profile icon on mobile and tablet

--- components/navigation/bottom-navigation.js
+++ components/navigation/bottom-navigation.js
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import cx from "clsx";
 import {
@@ -8,9 +10,18 @@ import {
   ViewGridIcon,
   UserCircleIcon,
 } from "@heroicons/react/solid";
+import { useSession } from "next-auth/react";
 
 function BottomNavigation() {
+  const [isSignOutOpen, setIsSignOutOpen] = useState(false);
+
   const { pathname } = useRouter();
+
+  const { data: session, status } = useSession();
+
+  const toggleIsSignOutOpen = () => {
+    setIsSignOutOpen(prevState => !prevState);
+  };
 
   return (
     <nav className="fixed bottom-0 left-0 z-20 w-full py-2 bg-black bg-opacity-90 xl:hidden">
@@ -73,10 +84,21 @@ function BottomNavigation() {
           })}>
           <Link href="/login">
             <a>
-              <div className="flex flex-col items-center justify-center space-y-1">
-                <UserCircleIcon className="w-6 h-6 md:w-8 md:h-8" />
-                <small>Profile</small>
-              </div>
+              {status === "unauthenticated" ? (
+                <div className="flex flex-col items-center justify-center space-y-1">
+                  <UserCircleIcon className="w-6 h-6 md:w-8 md:h-8" />
+                  <small>Sign In</small>
+                </div>
+              ) : (
+                <div className="relative w-8 h-8 md:w-10 md:h-10">
+                  <Image
+                    src={session.user.image}
+                    alt={session.user.name}
+                    layout="fill"
+                    className="rounded-full"
+                  />
+                </div>
+              )}
             </a>
           </Link>
         </li>
