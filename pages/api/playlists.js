6f71fb195045b50914c7db5bf87230124bd199a0
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-12-18 11:45:59 +0330

    Implemented endpoint for creating playlists

---
+++ pages/api/playlists.js
@@ -0,0 +1,52 @@
+import { getSession } from "next-auth/react";
+
+import clientPromise from "@lib/mongodb";
+
+export default async function handler(req, res) {
+  const { method } = req;
+
+  if (method === "POST") {
+    try {
+      const session = await getSession({ req });
+
+      if (!session) {
+        res
+          .status(401)
+          .json({ message: "Not authorized to access this endpoint" });
+      }
+
+      const client = await clientPromise;
+
+      const user = await client
+        .db()
+        .collection("users")
+        .findOne({ email: session.user.email });
+
+      if (!user) {
+        res.status(404).json({ message: "User not found" });
+      }
+
+      let playlists;
+
+      if (user.playlists) {
+        playlists = [...user.playlists, { [req.body]: [] }];
+      } else {
+        playlists = [{ [req.body]: [] }];
+      }
+
+      const query = { email: session.user.email };
+      const update = { $set: { playlists } };
+      const options = { returnDocument: "after" };
+
+      const updatedDocument = await client
+        .db()
+        .collection("users")
+        .findOneAndUpdate(query, update, options);
+
+      res.status(200).json({ message: "Success", updatedDocument });
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ message: error.message });
+    }
+  }
+}
