e3af443e1bd9e065d070d731714d2b90bbf16184
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-12-18 10:01:06 +0330

    Implemented playlist popup animation

--- components/playlist/playlist-popup.js
+++ components/playlist/playlist-popup.js
@@ -1,5 +1,35 @@
+import { motion } from "framer-motion";
+
+const dropIn = {
+  hidden: {
+    y: "100vh",
+    opacity: 0,
+  },
+  visible: {
+    y: "0",
+    opacity: 1,
+    transition: {
+      duration: 0.3,
+      type: "spring",
+      damping: 50,
+      stiffness: 500,
+    },
+  },
+  exit: {
+    y: "100vh",
+    opacity: 0,
+  },
+};
+
 function PlaylistPopup() {
-  return <div className="playlist-popup"></div>;
+  return (
+    <motion.div
+      variants={dropIn}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+      className="playlist-popup"></motion.div>
+  );
 }
 
 export default PlaylistPopup;
