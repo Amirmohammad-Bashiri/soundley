750cd5fdfec6340f5dbe0ec014f229eeaee9f01c
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-12-20 11:20:45 +0330

    Added optimistic update for adding and removing playlists

--- hooks/useCreatePlaylist.js
+++ hooks/useCreatePlaylist.js
@@ -6,7 +6,27 @@ export function useCreatePlaylist() {
   const queryClient = useQueryClient();
 
   return useMutation(createPlaylist, {
-    onSuccess: () => {
+    onMutate: async data => {
+      await queryClient.cancelQueries("user");
+      const previousValue = queryClient.getQueryData("user");
+
+      queryClient.setQueryData("user", old => {
+        if (old.playlists) {
+          return {
+            ...old,
+            playlists: [
+              ...old.playlists,
+              { id: Math.random() * 1000, name: data.playlistName, tracks: [] },
+            ],
+          };
+        }
+      });
+
+      return previousValue;
+    },
+    onError: (err, variables, previousValue) =>
+      queryClient.setQueryData("user", previousValue),
+    onSettled: () => {
       queryClient.invalidateQueries("user");
     },
   });
--- hooks/useRemovePlaylist.js
+++ hooks/useRemovePlaylist.js
@@ -6,7 +6,26 @@ export function useRemovePlaylist() {
   const queryClient = useQueryClient();
 
   return useMutation(removePlaylist, {
-    onSuccess: () => {
+    onMutate: async data => {
+      await queryClient.cancelQueries("user");
+      const previousValue = queryClient.getQueryData("user");
+
+      queryClient.setQueryData("user", old => {
+        if (old.playlists) {
+          return {
+            ...old,
+            playlists: old.playlists.filter(
+              playlist => playlist.id !== data.playlistId
+            ),
+          };
+        }
+      });
+
+      return previousValue;
+    },
+    onError: (err, variables, previousValue) =>
+      queryClient.setQueryData("user", previousValue),
+    onSettled: () => {
       queryClient.invalidateQueries("user");
     },
   });
