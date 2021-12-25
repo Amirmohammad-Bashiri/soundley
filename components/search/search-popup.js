06d723a60e07acb64a8fbb115683f5c2d9646bd9
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-12-25 12:52:37 +0330

    Responsive search popup

--- components/search/search-popup.js
+++ components/search/search-popup.js
@@ -44,7 +44,7 @@ function SearchPopup({ searchVal, setSearchVal }) {
       initial="hidden"
       animate="visible"
       exit="exit"
-      className="absolute z-10 left-0 w-[500px] max-h-[500px] overflow-y-scroll min-h-[500px] bg-gray-800 rounded top-20">
+      className="absolute z-10 left-5 sm:left-2 md:-left-20 xl:left-0 w-[250px] max-h-[300px] min-h-[300px] sm:w-[300px] sm:max-h-[400px] sm:min-h-[400px] md:w-[500px] md:max-h-[500px] overflow-y-scroll md:min-h-[500px] bg-black rounded top-20">
       <SearchPopupTabs activeTab={activeTab} setActiveTab={setActiveTab} />
 
       <SearchResults
