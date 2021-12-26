6beef858a065dd736b87a9ffda8696a44cffa08d
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-12-26 17:58:22 +0330

    Added padding on playlist

--- components/playlist/playlist.js
+++ components/playlist/playlist.js
@@ -2,7 +2,7 @@ import PlaylistItem from "./playlist-item";
 
 function Playlist({ data }) {
   return (
-    <ul className="w-full space-y-5 overflow-y-scroll max-h-[600px]">
+    <ul className="w-full py-8 space-y-5 overflow-y-scroll max-h-[600px]">
       {data.playlists.map(playlist => (
         <PlaylistItem key={playlist.id} playlist={playlist} />
       ))}
