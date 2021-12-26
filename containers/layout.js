36d6b0b39618449530ce967af5b5cc8a12e6c04f
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-12-26 18:21:29 +0330

    Lazy loaded layout components

--- containers/layout.js
+++ containers/layout.js
@@ -4,13 +4,8 @@ import cx from "clsx";
 import { useMediaQuery } from "react-responsive";
 
 import Header from "@components/navigation/header";
-import Sidebar from "@components/navigation/sidebar";
-import BottomNavigation from "@components/navigation/bottom-navigation";
-import GlobalMusicPlayer from "@components/global-player/global-music-player";
 import { usePlayer } from "@store/player-context";
 import { useUser } from "@hooks/useUser";
-import PlaylistPopup from "@components/playlist/playlist-popup";
-import PopupBackdrop from "@components/playlist/popup-backdrop";
 
 const MusicPlayerPortal = dynamic(() => import("../hoc/music-player-portal"), {
   ssr: false,
@@ -20,6 +15,25 @@ const MusicPlayerPopup = dynamic(
   { ssr: false }
 );
 const PlaylistPortal = dynamic(() => import("../hoc/playlist-portal"), {
+  ssr: false,
+});
+const PlaylistPopup = dynamic(
+  () => import("@components/playlist/playlist-popup"),
+  { ssr: false }
+);
+const PopupBackdrop = dynamic(
+  () => import("@components/playlist/popup-backdrop"),
+  { ssr: false }
+);
+const GlobalMusicPlayer = dynamic(
+  () => import("@components/global-player/global-music-player"),
+  { ssr: false }
+);
+const BottomNavigation = dynamic(
+  () => import("@components/navigation/bottom-navigation"),
+  { ssr: false }
+);
+const Sidebar = dynamic(() => import("@components/navigation/sidebar"), {
   ssr: false,
 });
 
@@ -36,9 +50,7 @@ function Layout({ children }) {
 
   return (
     <div>
-      <div className="hidden xl:block">
-        <Sidebar />
-      </div>
+      {isDesktopOrLaptop ? <Sidebar /> : null}
 
       <div className="relative bg-gray-900 xl:pl-52 2xl:pl-72">
         <Header />
@@ -64,7 +76,7 @@ function Layout({ children }) {
           </MusicPlayerPortal>
         ) : null}
 
-        <BottomNavigation />
+        {!isDesktopOrLaptop ? <BottomNavigation /> : null}
       </div>
     </div>
   );
