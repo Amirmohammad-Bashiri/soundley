23651c800c326557bb3f172df9bb9a7f6c5e23a5
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-12-26 09:11:46 +0330

    Changed genres layout

--- components/genres-page/genres-list.js
+++ components/genres-page/genres-list.js
@@ -2,7 +2,7 @@ import GenresListItem from "./genres-list-item";
 
 function GenresList({ data }) {
   return (
-    <ul className="flex flex-wrap items-center justify-center md:justify-start md:gap-x-5 gap-y-10">
+    <ul className="flex flex-col items-center justify-center md:grid md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 md:gap-x-5 gap-y-10">
       {data.map(genre => (
         <GenresListItem key={genre.id} genre={genre} />
       ))}
