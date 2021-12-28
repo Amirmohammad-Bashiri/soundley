7292ab3b802d18105df5ee348f11f73e37227d75
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-12-28 10:16:02 +0330

    Show top tracks if 50% of albums are visible

--- containers/home-page/grid-container.js
+++ containers/home-page/grid-container.js
@@ -1,3 +1,4 @@
+import { useInView } from "react-intersection-observer";
 import { ErrorBoundary } from "react-error-boundary";
 
 import TopArtists from "@components/home-page/artists/top-artists";
@@ -7,6 +8,8 @@ import DesktopPlayer from "@components/home-page/music-player/desktop-player";
 import ErrorFallback from "@components/error-fallback";
 
 function GridContainer() {
+  const { ref, inView } = useInView({ threshold: 0.5, triggerOnce: true });
+  console.log(inView);
   return (
     <div className="home-grid-layout">
       <section className="relative px-4 py-4 bg-gray-800 rounded xl:order-1 2xl:order-none xl:col-span-8 xl:row-span-3 xl:px-6">
@@ -28,7 +31,9 @@ function GridContainer() {
         <DesktopPlayer />
       </section>
 
-      <section className="relative overflow-y-scroll bg-gray-800 rounded xl:order-2 2xl:order-none xl:row-span-3 xl:col-span-4 2xl:col-span-3 max-h-80">
+      <section
+        ref={ref}
+        className="relative overflow-y-scroll bg-gray-800 rounded xl:order-2 2xl:order-none xl:row-span-3 xl:col-span-4 2xl:col-span-3 max-h-80">
         <h2 className="sticky top-0 z-10 py-4 pl-6 mb-6 text-xl font-semibold tracking-wide bg-gray-800 md:text-2xl text-gray-50">
           Top Albums
         </h2>
@@ -44,21 +49,23 @@ function GridContainer() {
         </ErrorBoundary>
       </section>
 
-      <section className="relative overflow-y-scroll bg-gray-800 rounded xl:order-4 2xl:order-none xl:row-span-3 xl:col-span-8 2xl:col-span-5 max-h-80">
-        <h2 className="sticky top-0 z-10 py-4 pl-6 mb-6 text-xl font-semibold tracking-wide bg-gray-800 md:text-2xl text-gray-50">
-          Top Tracks
-        </h2>
+      {inView ? (
+        <section className="relative overflow-y-scroll bg-gray-800 rounded xl:order-4 2xl:order-none xl:row-span-3 xl:col-span-8 2xl:col-span-5 max-h-80">
+          <h2 className="sticky top-0 z-10 py-4 pl-6 mb-6 text-xl font-semibold tracking-wide bg-gray-800 md:text-2xl text-gray-50">
+            Top Tracks
+          </h2>
 
-        <ErrorBoundary
-          fallbackRender={({ resetErrorBoundary }) => (
-            <ErrorFallback
-              resetErrorBoundary={resetErrorBoundary}
-              queryKey={["topTracks"]}
-            />
-          )}>
-          <TopTracks />
-        </ErrorBoundary>
-      </section>
+          <ErrorBoundary
+            fallbackRender={({ resetErrorBoundary }) => (
+              <ErrorFallback
+                resetErrorBoundary={resetErrorBoundary}
+                queryKey={["topTracks"]}
+              />
+            )}>
+            <TopTracks />
+          </ErrorBoundary>
+        </section>
+      ) : null}
     </div>
   );
 }
