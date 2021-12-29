6bca851e18d66e90b64efff12317eb1f4ff87ac2
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-12-29 10:16:05 +0330

    Removed desktop player with media package

--- containers/home-page/grid-container.js
+++ containers/home-page/grid-container.js
@@ -1,6 +1,7 @@
 import dynamic from "next/dynamic";
 import { useInView } from "react-intersection-observer";
 import { ErrorBoundary } from "react-error-boundary";
+import { useMediaQuery } from "react-responsive";
 
 import TopArtists from "@components/home-page/artists/top-artists";
 import TopAlbums from "@components/home-page/albums/top-albums";
@@ -20,6 +21,10 @@ const ErrorFallback = dynamic(() => import("@components/error-fallback"), {
 function GridContainer() {
   const { ref, inView } = useInView({ threshold: 0.5, triggerOnce: true });
 
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-width: 1280px)",
+  });
+
   return (
     <div className="home-grid-layout">
       <section className="relative px-4 py-4 bg-gray-800 rounded xl:order-1 2xl:order-none xl:col-span-8 xl:row-span-3 xl:px-6">
@@ -37,9 +42,11 @@ function GridContainer() {
         </ErrorBoundary>
       </section>
 
-      <section className="hidden py-4 bg-gray-800 rounded xl:order-3 2xl:order-none xl:row-span-3 2xl:row-span-6 xl:block xl:col-span-4 2xl:col-span-4">
-        <DesktopPlayer />
-      </section>
+      {isDesktopOrLaptop ? (
+        <section className="py-4 bg-gray-800 rounded xl:order-3 2xl:order-none xl:row-span-3 2xl:row-span-6 xl:col-span-4 2xl:col-span-4">
+          <DesktopPlayer />
+        </section>
+      ) : null}
 
       <section
         ref={ref}
