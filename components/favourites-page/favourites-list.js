05511bd26c79b6dbad85b1c43bb256ebf2714f49
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2022-01-01 11:07:03 +0330

    Removed inline style from favourites list

--- components/favourites-page/favourites-list.js
+++ components/favourites-page/favourites-list.js
@@ -2,9 +2,7 @@ import FavouritesListItem from "./favourites-list-item";
 
 function FavouritesList({ data }) {
   return (
-    <ul
-      style={{ maxHeight: "440px" }}
-      className="w-full px-5 pt-3 pb-5 space-y-4 overflow-y-scroll bg-gray-800 divide-y-2 divide-gray-700 rounded-sm md:px-10">
+    <ul className="w-full px-5 pt-3 pb-5 max-h-[440px] space-y-4 overflow-y-scroll bg-gray-800 divide-y-2 divide-gray-700 rounded-sm md:px-10">
       {data.likes.map(track => (
         <FavouritesListItem key={track.id} track={track} />
       ))}
