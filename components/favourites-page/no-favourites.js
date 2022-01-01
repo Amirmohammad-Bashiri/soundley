7e828c2962177c25088def49c86dc02f5066fe04
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2022-01-01 11:05:43 +0330

    Removed inline style from no favourites

--- components/favourites-page/no-favourites.js
+++ components/favourites-page/no-favourites.js
@@ -1,8 +1,6 @@
 function NoFavourites() {
   return (
-    <div
-      style={{ minHeight: "440px" }}
-      className="flex items-center justify-center px-5 pt-3 pb-5 bg-gray-800 rounded-sm md:px-10">
+    <div className="flex items-center min-h-[440px] justify-center px-5 pt-3 pb-5 bg-gray-800 rounded-sm md:px-10">
       <h1 className="text-4xl font-bold tracking-wide text-gray-50">
         No favorite yet...
       </h1>
