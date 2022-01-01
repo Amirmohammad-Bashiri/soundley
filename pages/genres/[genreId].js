482159e092afe951f32cdef47cb2e60593fbeb4b
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2022-01-01 10:10:33 +0330

    Added title to genres page

--- pages/genres/[genreId].js
+++ pages/genres/[genreId].js
@@ -1,5 +1,6 @@
+import Head from "next/head";
 import { useRouter } from "next/router";
-import { QueryClient, dehydrate } from "react-query";
+import { QueryClient, dehydrate, useQueryClient } from "react-query";
 import { deezerClient } from "@clients/deezer-client";
 import { soundleyClient } from "@clients/soundley-client";
 
@@ -10,20 +11,36 @@ import GenreArtistsList from "@components/genre-item-page/genre-artists-list";
 function GenreItemPage() {
   const { query } = useRouter();
 
+  const queryClient = useQueryClient();
+
+  const genres = queryClient.getQueryData("genres");
+
+  const genre = genres && genres.find(genre => genre.id === +query.genreId);
+
   const { data, isLoading } = useGenre(
     soundleyClient,
     `/genres/${query.genreId}`,
     query.genreId
   );
 
+  const pageTitle = genre
+    ? `Soundley | ${genre.name}`
+    : "Soundley | Listen to What You Love";
+
   if (isLoading) {
     return <h1 className="text-white">Loading...</h1>;
   }
 
   return (
-    <main className="pb-6 mx-6 mt-10 xl:pb-10 md:mt-14">
-      <GenreArtistsList artists={data.data} />
-    </main>
+    <>
+      <Head>
+        <title>{pageTitle}</title>
+      </Head>
+
+      <main className="pb-6 mx-6 mt-10 xl:pb-10 md:mt-14">
+        <GenreArtistsList artists={data.data} />
+      </main>
+    </>
   );
 }
 
